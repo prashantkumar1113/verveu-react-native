f2006757a81decc875a1b50ac1fff29ea74bb1c5
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-11-19 16:11:02 -0600

    Added some core components to or ToDo App

--- react-native-starter/App.js
+++ react-native-starter/App.js
@@ -1,20 +1,29 @@
-import {StatusBar} from "expo-status-bar";
-import {StyleSheet, Text, View} from "react-native";
+// import {StatusBar} from "expo-status-bar";
+import {StyleSheet, Text, View, Button, TextInput} from "react-native";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>VerveU starter App!</Text>
-            <StatusBar style="auto" />
+        <View style={styles.appContainer}>
+            {/* Adding a todo */}
+            <View>
+                <TextInput placeholder="Your goals" />
+                <Button title="Add Goal" />
+            </View>
+
+            {/* List of todos */}
+            <View>
+                <Text>List of Goals....</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "azure",
-        alignItems: "center",
-        justifyContent: "center",
+    appContainer: {
+        // flex: 1,
+        // backgroundColor: "azure",
+        // alignItems: "center",
+        // justifyContent: "center",
+        padding: 50,
     },
 });
